9ac8921c8a40da0ee43ea2f1ee8679e80639e1e4
shihas <shihasbacker@yahoo.com>
2020-08-05 13:43:15 +0530

    jquery completed

--- js/fb.js
+++ js/fb.js
@@ -3,9 +3,36 @@ $(document).ready(function(){
         rules: {
             fname:{
                 required:true,
+                minlength:4,
+                maxlength:6
+            },
+            sname: {
+                required:true,
                 minlength:4
-            }
+            },
+            emailAddress:{
+                required:true,
+                email:true
+            },
+            password:{
+                required:true,
+                minlength: 6
+            },
         },
+        messages:{
+            fname: "enter firstname"
+            //required: "this field should be requir"
+        }
+    })
+    $("form[id='side-heading']").validate({
+        rules:{
+            day: {
+                required:true
+            },
+            gender:{
+                required:true
+            }
+        }
     })
 })
 
